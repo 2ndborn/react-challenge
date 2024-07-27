0afdb9c793ac93220bc1dbc409e9e17368ffdae3
Micah Francis <micah.francis69@googlemail.com>
2024-07-27 17:40:08 +0000

    Rendering lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import css from "./components/css/Sidebar.module.css";
 import Sidebar from "./components/Sidebar";
 import NavBarSimple from "./components/NavBarSimple";
+import NavBarForm from "./components/NavBarForm"
+import Content from "./components/Content"
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       {/* Add your components here */}
       <Sidebar />
       <NavBarSimple />
+      <NavBarForm />
+      <Content />
     </div>
   );
 }
