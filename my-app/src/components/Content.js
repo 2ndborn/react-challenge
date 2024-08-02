5ea949d144f55fb6b50e9a48879daf0f8872038b
Micah Francis <micah.francis69@googlemail.com>
2024-08-02 05:20:22 +0000

    Search bar challenge completed

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
-import css from "./css/Content.module.css"
-import { savedPosts } from "../posts.json"
-import PostItem from './PostItem'
-import Loader from './Loader'
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
     constructor(props) {
-      super(props)
-      this.state = {
-        isLoaded: false,
-        posts: []
-      }
+        super(props)
+        this.state = {
+            isLoaded: false,
+            posts: [],
+        }
     }
 
     componentDidMount() {
-        setTimeout(() => {
+        setTimeout(()=>{
             this.setState({
                 isLoaded: true,
                 posts: savedPosts,
@@ -22,12 +22,12 @@ export class Content extends Component {
         }, 2000)
     }
 
-    handleChange = (event) => {
-        const inputText = event.target.value.toLowerCase();
-        console.log(inputText)
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
         const filteredPosts = savedPosts.filter((post)=>{
-            return post.inputText.toLowerCase().includes(inputText);
+            return post.name.toLowerCase().includes(name);
         })
+        
         this.setState({
             posts: filteredPosts
         })
@@ -36,22 +36,27 @@ export class Content extends Component {
     render() {
         return (
             <div className={css.Content}>
+                
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
                     <form>
-                        <label htmlFor='searchInput' id="searchInput">Search: </label>
+                        <label htmlFor='searchinput'>Search</label>
                         <input 
-                        onChange={(event) => this.handleChange(event)}
                         type='search' 
-                        id="searchInput" 
+                        id='searchinput' 
+                        placeholder='By Author'
+                        onChange={(e) => this.handleChange(e)}
                         />
+                        <h4>posts found {this.state.posts.length}</h4>
                     </form>
-                    <h4>Posts Found: {this.state.posts.length}</h4>
                 </div>
+
                 <div className={css.SearchResults}>
-                    {this.state.isLoaded ?
-                    <PostItem savedPosts={savedPosts} />
-                    : <Loader />}
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={this.state.posts} />
+                        : <Loader />
+                    }
                 </div>
             </div>
         )
