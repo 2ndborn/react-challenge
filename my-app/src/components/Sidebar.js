2408aea41df858e5bf6aeae012e8d4edb971da0c
2ndborn <micah.francis69@googlemail.com>
2025-02-05 06:34:58 +0000

    Added the a tags for the navigation menu

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+    return (
+        <div>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+        </div>
+    )
+};
