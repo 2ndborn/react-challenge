d39cc3a25403e4ca240a035b62e878a76f96a584
2ndborn <micah.francis69@googlemail.com>
2025-02-12 16:26:28 +0000

    Loader challenge complete

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,20 +2,40 @@ import React, { Component } from 'react'
 import {savedPosts} from '../posts.json'
 import PostItem from './PostItem'
 import css from './css/Content.module.css'
+import Loader from './Loader'
 
 export class Content extends Component {
   constructor(props) {
+    console.log('constructor called')
     super(props)
-    
+    this.state = {
+      isLoaded: false
+    }
   }
+
+  componentDidMount() {
+    console.log('getData called')
+    setTimeout(() => {
+      console.log('data fetched')
+      this.setState({
+        isloaded: true
+      })
+    }, 2000)
+  }
+
   render() {
+    console.log('render called')
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}>
           <h1>My Photos</h1>
         </div>
         <div className={css.SearchResults}>
-          <PostItem savedPosts={savedPosts} />
+          {this.state.isloaded ? (
+            <PostItem savedPosts={savedPosts} />
+          ) : (
+            <Loader />
+          )}
         </div>
       </div>
     )
