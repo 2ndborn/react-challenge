83e8257872cdba46e47435fcd05e52a13c15ef77
2ndborn <micah.francis69@googlemail.com>
2025-03-22 11:05:38 +0000

    Converted the contentAPI with hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import css from './App.module.css';
 import Content from './components/Content';
 import ContentAPI from './components/ContentAPI';
+import ContentAPIHooks from './components/ContentAPIHooks';
 import ContentHooks from './components/ContentHooks';
 import NavBarForm from './components/NavBarForm';
 import NavBarSimple from './components/NavBarSimple';
@@ -10,7 +11,7 @@ function App() {
     <div className={css.App}>
       {/* <NavBarSimple /> */}
       <NavBarForm />
-      <ContentAPI />
+      <ContentAPIHooks />
     </div>
   );
 }
---
+++ my-app/src/components/ContentAPIHooks.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import css from './css/Content.module.css'
+import Loader from './Loader'
+import axios from 'axios'
+import API_KEY from '../secrets'
+import PostItemAPI from './PostItemAPI'
+
+function ContentAPIHooks() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [savedPosts, setSavedPosts] = useState([]);
+
+  useEffect(() => {
+    fetchImages()
+  }, [])
+
+  const fetchImages = async () => {
+    const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&per_page=100`);
+    console.log(response)
+    const fetchedPosts = response.data.hits;
+    setIsLoaded(true)
+    setPosts(fetchedPosts)
+    setSavedPosts(fetchedPosts)
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.value.toLowerCase()
+    const filteredPosts = savedPosts.filter((post) => 
+      post.user.toLowerCase().includes(name))
+    setPosts(filteredPosts)
+  }
+
+  console.log('render called')
+  return (
+    <div className={css.Content}>
+      <div className={css.TitleBar}>
+        <h1>My Photos</h1>
+        <label htmlFor='searchInput'>Search: </label>
+        <input
+          onChange={(event) => handleChange(event)}
+          type='search'
+          id='searchInput'
+          placeholder='By Author'
+        />
+        <h4>Posts found: {posts.length}</h4>
+      </div>
+      <div className={css.SearchResults}>
+        {isLoaded ? (
+          <PostItemAPI savedPosts={posts} />
+        ) : (
+          <Loader />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default ContentAPIHooks
